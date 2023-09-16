c7823f9636d233c6a5a642f7cd8d7292ae78f78f
Calix-Mingyu-Kim <calixkim58@gmail.com>
2023-09-16 16:31:11 -0400

    log files

---
+++ src/logger.ts
@@ -0,0 +1,32 @@
+const {createLogger, format, transports} = require("winston");
+const { combine, timestamp, json, prettyPrint } = format;
+
+require("dotenv").config();
+console.log(`Hello: ${process.env.LOG_FILE}`)
+
+const logger = createLogger({
+  format: combine(timestamp(), json(), prettyPrint()),
+  transports: [
+    new transports.File({
+      filename: `${process.env.LOG_FILE}`,
+      level: `${process.env.LOG_LEVEL}`
+    }),
+  ],
+});
+
+export default logger;
+
+
+/*
+// Usage: winston allows you to define a level property on each transport which specifies the maximum level of messages that a transport should log.
+// Logger configuration is set in the logger.ts file using process.env. (e.g. if LOG_LEVEL=1 (i.e. info), then logger.debug() will not be logged)
+
+if (process.env.LOG_LEVEL != 0) {
+// if informational message:
+    logger.info(message)
+// if debug message:
+    logger.debug(message)
+}
+
+*/
+
