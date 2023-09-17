746c15d1606c27a13c8e989887e9b0f744157035
XavierJCallait <113396487+XavierJCallait@users.noreply.github.com>
2023-09-17 15:27:04 -0400

    Added comments

--- src/fill-models.ts
+++ src/fill-models.ts
@@ -8,17 +8,24 @@ import {
 } from './middleware/metric-calculations'
 import {Urlmetrics} from './url-models'
 
+// using imported class and functions to get scores and send to frontend
 export function assignMetrics(data: string): Urlmetrics {
   const newURL = new Urlmetrics(data)
-  newURL.BusFactor = calculateBusFactor(newURL)
-  newURL.Correctness = calculateCorrectness(newURL)
-  newURL.RampUp = calculateRampUpTime(newURL)
-  newURL.Responsiveness = calculateResponsiveness(newURL)
-  newURL.License = calculateLicenseCompliance(newURL)
-  newURL.NetScore = calculateNetScore(newURL)
+  newURL.BusFactor = calculateBusFactor(newURL.URL)
+  newURL.Correctness = calculateCorrectness(newURL.URL)
+  newURL.RampUp = calculateRampUpTime(newURL.URL)
+  newURL.Responsiveness = calculateResponsiveness(newURL.URL)
+  newURL.License = calculateLicenseCompliance(newURL.URL)
+  newURL.NetScore = calculateNetScore(newURL.URL)
   return newURL
 }
 
-export function outputMetrics(data: Urlmetrics): void {
-  console.log(data.makeNDJSON())
+// exporting for unit tests
+export {
+  calculateBusFactor,
+  calculateCorrectness,
+  calculateRampUpTime,
+  calculateResponsiveness,
+  calculateLicenseCompliance,
+  calculateNetScore, 
 }
--- src/url-models.ts
+++ src/url-models.ts
@@ -1,4 +1,6 @@
+// class to be used for the frontend and outputting of metrics
 export class Urlmetrics {
+  // fields to store the different metrics and URL
   URL!: string
   BusFactor!: number
   Correctness!: number
@@ -7,11 +9,13 @@ export class Urlmetrics {
   License!: number
   NetScore!: number
 
+  // fill in the field for the URL when creating the class
   constructor(URL: string) {
     this.URL = URL
   }
 
-  makeNDJSON(): string {
+  // make JSON format of class
+  makeNSON(): string {
     return JSON.stringify(this)
   }
 }
