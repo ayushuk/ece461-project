edfc721e50b5c44b5355400bc8bfbebd3e06c472
Calix-Mingyu-Kim <calixkim58@gmail.com>
2023-09-21 19:11:34 -0400

    checkURL update

--- src/commands/checkUrl.ts
+++ src/commands/checkUrl.ts
@@ -1,4 +1,6 @@
 import {Args, Command, Flags} from '@oclif/core'
+import * as fs from 'fs'
+import { assignMetrics } from './src/fill-models';
 
 export default class CheckUrl extends Command {
   static description = 'describe the command here'
@@ -12,30 +14,19 @@ export default class CheckUrl extends Command {
 
   public async run(): Promise<void> {
     const {args} = await this.parse(CheckUrl)
-    const fs = require('fs')
     const hiddenPath = './check_install'
     const fileContent = fs.readFileSync(hiddenPath, 'utf-8')
     if (fileContent == 'yes\n') {
-      const rl = fs.readline.createInterface({
-        input: fs.createReadStream(args),
-        output: process.stdout,
-        terminal: false,
-      })
-      rl.on('line', (url: string) => {
-        const Metrics = {
-          URL: url,
-          netScore: 0,
-          rampUp: 0,
-          correctness: 0,
-          busFactor: 0,
-          respMaintain: 0,
-          license: 0,
-        }
+      const allFileContents = fs.readFileSync(args.urls, 'utf-8')
+      allFileContents.split(/\r?\n/).forEach((url) => {
 
-        console.log(`{"URL": "${url}", "NET SCORE":${Metrics.netScore}, "RAMP_UP_SCORE":${Metrics.rampUp},
-        "CORRECTNESS_SCORE":${Metrics.correctness}, "BUS_FACTOR_SCORE":${Metrics.busFactor},
-        "RESPONSIVE_MAINTAINER_SCORE":${Metrics.respMaintain}, "LICENSE_SCORE":${Metrics.license}}`)
+        const Metrics = assignMetrics(url);
+
+        console.log(
+          `{"URL": "${url}", "NET SCORE":${Metrics.netScore}, "RAMP_UP_SCORE":${Metrics.rampUp}, "CORRECTNESS_SCORE":${Metrics.correctness}, "BUS_FACTOR_SCORE":${Metrics.busFactor}, "RESPONSIVE_MAINTAINER_SCORE":${Metrics.respMaintain}, "LICENSE_SCORE":${Metrics.license}}`,
+        )
       })
+
       process.exit(0)
     } else {
       console.log(
