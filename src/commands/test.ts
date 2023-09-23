be32a5b0392f6ccd8d9d382f248c33ce6392e1c2
Mingyu Kim <kim3118@eceprog4.ecn.purdue.edu>
2023-09-23 10:27:30 -0400

    test fixed

--- src/commands/test.ts
+++ src/commands/test.ts
@@ -10,26 +10,19 @@ export default class Test extends Command {
     const fileContent = fs.readFileSync(hiddenPath, 'utf-8')
     if (fileContent == 'yes\n') {
       exec('npm test', (error: string, stdout: string) => {
-        console.log(stdout)
-        const passRegex = /(\d+) passing/
-        const failRegex = /(\d+) failing/
-        const passMatch = passRegex.exec(stdout)
-        const failMatch = failRegex.exec(stdout)
-        const passTests = passMatch ? Number.parseInt(passMatch[1], 10) : 0
-        const failTests = failMatch ? Number.parseInt(failMatch[1], 10) : 0
-        const totalTests = passTests + failTests
-        const covRegex =
-          /All files\s+\|\s+(\d+(?:\.\d+)?|\d{2,3})\s+\|\s+(\d+(?:\.\d+)?|\d{2,3})\s+\|\s+(\d+(?:\.\d+)?|\d{2,3})\s+\|\s+(\d+(?:\.\d+)?|\d{2,3})/
-        const covMatch = covRegex.exec(stdout)
-        const coverage = covMatch ? Number.parseFloat(covMatch[4]) : 0
+        const testsRegex = /(\d+) failed, (\d+) passed, (\d+) total/;
+        const testsMatch = testsRegex.exec(error)
 
-        console.log(`Total: ${totalTests}`)
-        console.log(`Passed: ${passTests}`)
-        console.log(`Coverage: ${coverage}%`)
-        console.log(
-          `${passTests}/${totalTests} test cases passed. ${coverage}% line coverage achieved.`,
-        )
+        if (testsMatch) {
+          const failedTests = parseInt(testsMatch[1]);
+          const passedTests = parseInt(testsMatch[2]);
+          const totalTests = parseInt(testsMatch[3]);
+          console.log(
+            `${passedTests}/${totalTests} test cases passed. ${passedTests / totalTests * 100}% line coverage achieved.`,
+          )
+        }
       })
+      process.exit(0)
     } else {
       console.log(
         'Dependencies not yet installed. Please run the following command:\n./run install',
