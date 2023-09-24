ac0328918d4df41637d2e17315acd5afb2e17690
Patrick McLin <pmclin@purdue.edu>
2023-09-24 15:31:14 -0400

    test fix

--- check/helpers/init.js
+++
@@ -1,7 +0,0 @@
-const path = require('path')
-
-process.env.TS_NODE_PROJECT = path.resolve('check/tsconfig.json')
-process.env.NODE_ENV = 'development'
-
-global.oclif = global.oclif || {}
-global.oclif.columns = 80
---
+++ test/commands/tester.test.ts
@@ -0,0 +1,16 @@
+import {test} from '@oclif/test'
+import * as fs from 'fs'
+
+describe('Test', () => {
+  //const check = jest.spyOn(fs, 'readFileSync').mockReturnValue('yes\n')
+
+  test
+    .stdout()
+    .command(['tester'])
+    .it('runs Test', (ctx) => {
+      expect(ctx.stdout).toContain('line coverage achieved.')
+      console.log(`Received output: ${ctx}`)
+    })
+
+  //check.mockRestore()
+})
