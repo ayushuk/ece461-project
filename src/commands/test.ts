7e12d72e664fb9ca24851bc9e54546f589b34c5d
Mingyu Kim <kim3118@eceprog1.ecn.purdue.edu>
2023-09-23 20:53:21 -0400

    Test.ts fixed

--- src/commands/test.ts
+++ src/commands/test.ts
@@ -9,23 +9,23 @@ export default class Test extends Command {
     const fs = require('fs')
     const fileContent = fs.readFileSync(hiddenPath, 'utf-8')
     if (fileContent == 'yes\n') {
-      exec('npm test', (error: string, stdout: string, stderr: string) => {
-        const testRegex = /(\d+) failed, (\d+) passed, (\d+) total/
+      exec('npx jest --config ./jest.config.ts', (error: string, stdout: string, stderr: string) => {
+        const testRegex = /Tests:\s+(\d+) failed,\s+(\d+) passed,\s+(\d+) total/
         const testMatch = testRegex.exec(stderr)
         if (testMatch) {
           const passTests = parseInt(testMatch[2], 10)
-          const totalTests = parseInt(testMatch[3], 10)
+          const failTests = parseInt(testMatch[1], 10)
+          const totalTests = passTests + failTests
           const coverage = passTests / totalTests * 100
           console.log(
-            `${passTests}/${totalTests} test cases passed. ${coverage.toFixed(0)}% line coverage achieved.`,
+            `${passTests}/${totalTests} test cases passed. ${coverage.toFixed(0)}% line coverage achieved.`
           )
         } else {
           console.log(
-            `0 test cases passed. 0% line coverage achieved.`,
+            `0/0 test cases passed. 0% line coverage achieved.`,
           )
         }
       })
-      process.exit(0)
     } else {
       console.log(
         'Dependencies not yet installed. Please run the following command:\n./run install',
