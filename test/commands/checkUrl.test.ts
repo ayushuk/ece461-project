3c288d9d9486a1a2d548de73e32b5a80d4002e9f
XavierJCallait <113396487+XavierJCallait@users.noreply.github.com>
2023-09-24 21:40:17 -0400

    Updating tests

--- test/commands/checkUrl.test.ts
+++ test/commands/checkUrl.test.ts
@@ -1,33 +1,21 @@
-import {test} from '@oclif/test'
-import {jest, expect} from '@jest/globals'
+// Import your Oclif command
+import CheckUrl from '../../src/commands/checkUrl';
 
-describe('check_url', () => {
-  it('should return scores in NDJSON format', async () => {
-    const { assignMetrics } = require('../src/fill-models')
-    const { Urlmetrics } = require('../src/url-models')
-    const metrics = new Urlmetrics('https://github.com/user/repo')
-    metrics.BusFactor = 0.5
-    metrics.Correctness = 0.4
-    metrics.RampUp = 0.3
-    metrics.Responsiveness = 0.2
-    metrics.License = 1
-    metrics.NetScore = 0.6
-    assignMetrics.mockReturnValue(metrics)
+describe('CheckUrl Command', () => {
+    it('runs without dependencies', async () => {
+      const fs = require('fs')
+      jest.spyOn(fs, 'readFileSync').mockImplementation(function () {
+        return 'no\n'
+      })
+      jest.spyOn(console, 'log')
+    
+      // call the command, ensure the file exists
+      await CheckUrl.run(['./one-url.txt'])
 
-    expect(stdout).toContain('{"URL": "https://github.com/user/repo", "NET_SCORE": 0.6, "RAMP_UP_SCORE":0.3, "CORRECTNESS_SCORE":0.4, "BUS_FACTOR_SCORE":0.5, "RESPONSIVE_MAINTAINER_SCORE":0.2, "LICENSE_SCORE":1}')
-  })
+      expect(console.log).toHaveBeenCalledWith(
+        'Dependencies not yet installed. Please run the following command:\n./run install'
+      );
 
-  test
-    .stdout()
-    .command(['check_url', '--name', 'jeff'])
-    .it('runs hello --name jeff', (ctx) => {
-      expect(ctx.stdout).to.contain('hello jeff')
+      expect(process.exit).toHaveBeenCalledWith(1);
     })
-
-  test
-    .stdout()
-    .command([''])
-    .it('runs check_url without passing in a URL', (ctx) => {
-      expect(ctx.stdout).to.contain('')
-  })
-})
+});
