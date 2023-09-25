06ac066459a7857de9f5ea105abc1a3d4e03399a
Patrick Thomas McLin <pmclin@eceprog2.ecn.purdue.edu>
2023-09-24 22:11:06 -0400

    initial test test

---
+++ test/commands/test.test.ts
@@ -0,0 +1,15 @@
+import {test} from '@oclif/test'
+import * as fs from 'fs'
+
+describe('Test', () => {
+  const check = jest.spyOn(fs, 'readFileSync').mockReturnValue('yes\n')
+
+  test
+    .stdout()
+    .command(['tester'])
+    .it('runs Test', (ctx) => {
+      expect(ctx.stdout).toContain('')
+    })
+
+  check.mockRestore()
+})
