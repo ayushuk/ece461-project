a9eb9f941f7c1222454ba5901910b10e1039e094
Calix-Mingyu-Kim <calixkim58@gmail.com>
2023-09-24 21:04:05 -0400

    Unit test for install.ts

--- test/commands/install.test.ts
+++ test/commands/install.test.ts
@@ -2,8 +2,7 @@ import {command, test} from '@oclif/test'
 import fs from 'node:fs'
 import {readFileAsync, Install} from '../../src/commands/install'
 import logger from '../../src/logger'
-import { exec } from 'node:child_process'
-
+import {exec} from 'node:child_process'
 
 describe('readFileAsync', () => {
   it('should read a file successfully', async () => {
@@ -11,9 +10,9 @@ describe('readFileAsync', () => {
     const result = JSON.parse(await readFileAsync(filePath))
     expect(result.dependencies).toBeDefined()
   })
-  
+
   it('should log an error if file reading fails', async () => {
-    const loggerSpy = jest.spyOn(logger, 'info').mockImplementation();
+    const loggerSpy = jest.spyOn(logger, 'info').mockImplementation()
     const nonExistentFilePath = 'non-existent-file.json'
     let error: any
     try {
@@ -23,21 +22,21 @@ describe('readFileAsync', () => {
       expect(loggerSpy).toHaveBeenCalled()
       await expect(
         readFileAsync(nonExistentFilePath),
-        ).rejects.toThrowErrorMatchingSnapshot()
-      }
-      loggerSpy.mockRestore()
-    })
+      ).rejects.toThrowErrorMatchingSnapshot()
+    }
+    loggerSpy.mockRestore()
   })
-  
+})
+
 describe('Install', () => {
   it('should successfully run the install command', async () => {
-    const packagePath = "./package.json"
-    
-    const consoleLogMock = jest
-    .spyOn(process.stdout, 'write')
-    .mockImplementation()
+    const packagePath = './package.json'
 
-  // Call the async run() function
+    const consoleLogMock = jest
+      .spyOn(process.stdout, 'write')
+      .mockImplementation()
+
+    // Call the async run() function
     await Install.run(['install'])
 
     const logCalls = consoleLogMock.mock.calls
