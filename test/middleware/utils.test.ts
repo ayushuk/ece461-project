67d4e1b587dd8291f3afaf831da3c8086b78a292
Evan Allender <evan.allender@gmail.com>
2023-09-24 20:07:21 -0400

    linter fixes

--- test/middleware/utils.test.ts
+++ test/middleware/utils.test.ts
@@ -1,28 +1,28 @@
 // import {parse} from 'path'
-// import * as utils from '../../src/middleware/utils'
-import {
-  round,
-  identifyLink,
-  cloneRepo,
-  parseGHRepoName,
-  getLinesOfCode,
-  calcRepoLines,
-} from '../../src/middleware/utils'
+import * as utils from '../../src/middleware/utils'
+// import {
+//   round,
+//   identifyLink,
+//   cloneRepo,
+//   parseGHRepoName,
+//   getLinesOfCode,
+//   calcRepoLines,
+// } from '../../src/middleware/utils'
 
 describe('round', () => {
   // Test case 1: Round down 2 decimal places
   it('should return 768.98 given 768.983', () => {
-    expect(round(768.983, 2)).toBe(768.98)
+    expect(utils.round(768.983, 2)).toBe(768.98)
   })
 
   // Test case 2: Round up 7 decimal places
   it('should return 768.98 given 768.983', () => {
-    expect(round(768.983_987_254, 7)).toBe(768.983_987_3)
+    expect(utils.round(768.983_987_254, 7)).toBe(768.983_987_3)
   })
 
   // Test case 2: Round negative number
   it('should return 768.98 given 768.983', () => {
-    expect(round(-5.129, 2)).toBe(-5.13)
+    expect(utils.round(-5.129, 2)).toBe(-5.13)
   })
 })
 
@@ -31,21 +31,21 @@ describe('identifyLink', () => {
   it('Should return github when given a valid github link', () => {
     const testUrl = 'https://github.com/ayushuk/ece461-project'
 
-    expect(identifyLink(testUrl)).toBe('github')
+    expect(utils.identifyLink(testUrl)).toBe('github')
   })
 
   // test case 2: valid npm link
   it('Should return npm when given a valid npm link', () => {
     const testUrl = 'https://npmjs.com/package/express'
 
-    expect(identifyLink(testUrl)).toBe('npm')
+    expect(utils.identifyLink(testUrl)).toBe('npm')
   })
 
   // test case 3: invalid link
   it('Should return null when given an invalid link', () => {
     const testUrl = 'https://google.com'
 
-    expect(identifyLink(testUrl)).toBe(null)
+    expect(utils.identifyLink(testUrl)).toBe(null)
   })
 })
 
@@ -53,7 +53,7 @@ describe('parseGHRepoName', () => {
   it('should return the repo name when given a valid github link', () => {
     const testUrl = 'https://github.com/octocat/Spoon-Knife'
 
-    expect(parseGHRepoName(testUrl)).toBe('Spoon-Knife')
+    expect(utils.parseGHRepoName(testUrl)).toBe('Spoon-Knife')
   })
 })
 
@@ -65,7 +65,7 @@ describe('parseGHRepoName', () => {
 
 //     cloneRepo(testUrl)
 //     // TODO: fix this test
-//     expect(cloneRepo(testUrl))
+//     expect(utils.cloneRepo(testUrl))
 //   })
 //   it('should clone a github repo', () => {
 //     const testUrl = 'https://github.com/cloudinary/cloudinary_npm'
@@ -73,7 +73,7 @@ describe('parseGHRepoName', () => {
 
 //     cloneRepo(testUrl)
 //     // TODO: fix this test
-//     expect(cloneRepo(testUrl))
+//     expect(utils.cloneRepo(testUrl))
 //   })
 // })
 
@@ -83,7 +83,7 @@ describe('parseGHRepoName', () => {
 //     const testFilePath =
 //       '../ece461-project/src/middleware/cloned-repos/Spoon-Knife/README.md'
 
-//     const result = await getLinesOfCode(testFilePath)
+//     const result = await utils.getLinesOfCode(testFilePath)
 
 //     expect(result).toBe(10)
 //   })
@@ -94,7 +94,7 @@ describe('parseGHRepoName', () => {
 //     const testRepoPath =
 //       '../ece461-project/src/middleware/cloned-repos/Spoon-Knife'
 
-//     const result = await calcRepoLines(testRepoPath)
+//     const result = await utils.calcRepoLines(testRepoPath)
 
 //     expect(result).toBe(49)
 //   })
